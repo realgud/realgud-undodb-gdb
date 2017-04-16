e290685a9f0ac936e16f9533621eed2754ce53ad
rocky <rb@dustyfeet.com>
2017-04-16 10:25:49 -0400

    Improve README text

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 # From inside emacs, make sure test-simple is installed and then
 # Press C-x C-e at the end of the next line run this script
 # (test-simple-run "EMACSLOADPATH=%s ./autogen.sh"  (mapconcat 'identity load-path ":"))
-ln -fs README.adoc README
+ln -fs README.md README
 touch common.mk
 autoreconf -vfi && \
 autoconf && {
